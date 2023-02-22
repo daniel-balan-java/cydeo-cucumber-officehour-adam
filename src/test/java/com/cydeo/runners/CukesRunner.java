2b9848b844c4411628c400a6d1d3064d8f664d94
Daniel_B <daniel.balan1995@gmail.com>
2023-02-22 12:42:23 -0500

    project is created

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html"
+
+
+        }, // specially for reports
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        tags = "@validLogin",
+        dryRun = false, // in order to get snippets
+        publish = true // in order to generate public link for reports
+
+)
+public class CukesRunner {
+}
